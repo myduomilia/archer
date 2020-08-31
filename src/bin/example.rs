2d70cc684c7cb56c154455ab76f1a024ef8a4741
myduomilia <myduomilia@gmail.com>
2020-09-01 00:39:26 +0400

    Bug fix check close connection

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,10 +1,10 @@
-extern crate archer;
+extern crate archers;
 use env_logger::Env;
 use log::{error};
 
 fn main() {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    while let Err(err) = archer::run("0.0.0.0:8080") {
+    if let Err(err) = archers::run("0.0.0.0:8080") {
         error!("{}", err.to_string());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
-use async_std::task;
+use async_std::{task, io};
+use async_listen::{ListenExt, error_hint};
 use log::{debug, error, info};
-use std::{fs};
 use std::sync::Arc;
 use std::option::Option::Some;
 use std::collections::{HashMap};
-use std::path::Path;
-use std::time::{Duration, Instant};
-use async_timer::timed;
-use async_std::io::ErrorKind;
+use std::time::Duration;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 type Sender<T> = async_channel::Sender<T>;
@@ -20,43 +17,51 @@ struct Request {
 }
 
 enum Event {
-    Connection {
+    OpenConnection {
         stream: Arc<TcpStream>,
     },
-    ReadyResponse {
+    SendingData {
         connection_id: u32,
-        payload: Arc<Vec<u8>>,
+        payload: Vec<u8>,
+    },
+    PackageReceived {
+        connection_id: u32,
+        payload: Vec<u8>,
     },
     TimerCheckAliveConnection,
     CloseConnection {
         stream: Arc<TcpStream>,
         connection_id: u32,
     },
-    CompletedResponse {
+    Disconnect {
         connection_id: u32,
     },
 }
 
-fn spawn_write<F>(fut: F, tx_ev: Sender<Event>, key: u32) -> task::JoinHandle<(Result<()>)>
+fn log_accept_error(e: &io::Error) {
+    error!("Error: {}. Listener paused for 0.5s. {}", e, error_hint(e))
+}
+
+fn spawn_write<F>(fut: F, tx_ev: Sender<Event>, key: u32) -> task::JoinHandle<Result<()>>
     where
         F: Future<Output = Result<()>> + Send + 'static,
 {
     task::spawn(async move {
         match fut.await {
             Ok(_) => {
-                tx_ev.send(Event::CompletedResponse {connection_id: key}).await?;
+                tx_ev.send(Event::Disconnect {connection_id: key}).await?;
             },
             Err(err) => {
-                error!("{}", err.to_string());
+                error!("Operation write error. {}", err.to_string());
             }
         }
         Ok(())
     })
 }
 
-fn spawn_read<F>(fut: F, tx_ev: Sender<Event>, key: u32, stream: Arc<TcpStream>) -> task::JoinHandle<(Result<()>)>
+fn spawn_read<F>(fut: F, tx_ev: Sender<Event>, key: u32, stream: Arc<TcpStream>) -> task::JoinHandle<Result<()>>
     where
-        F: Future<Output = Result<(usize)>> + Send + 'static,
+        F: Future<Output = Result<usize>> + Send + 'static,
 {
     task::spawn(async move {
         match fut.await {
@@ -66,7 +71,7 @@ fn spawn_read<F>(fut: F, tx_ev: Sender<Event>, key: u32, stream: Arc<TcpStream>)
                 }
             }
             Err(err) => {
-                error!("{}", err.to_string());
+                error!("Operation read error. {}", err.to_string());
                 tx_ev.send(Event::CloseConnection {stream, connection_id: key}).await?;
             }
         }
@@ -74,12 +79,12 @@ fn spawn_read<F>(fut: F, tx_ev: Sender<Event>, key: u32, stream: Arc<TcpStream>)
     })
 }
 
-async fn broker_loop(rx_req: Sender<Request>, mut tx_ev: Sender<Event>, mut rx_ev: Receiver<Event>) -> Result<()> {
+async fn broker_loop(rx_req: Sender<Request>, tx_ev: Sender<Event>, mut rx_ev: Receiver<Event>) -> Result<()> {
     let mut idx = 0;
     let mut connections: HashMap<u32, Arc<TcpStream>> = HashMap::new();
     while let Some(event) = rx_ev.next().await {
         match event {
-            Event::Connection {stream} => {
+            Event::OpenConnection {stream} => {
                 debug!("Number of connected clients = {}", connections.len());
                 connections.insert(idx, stream.clone());
                 if let Err(err) = rx_req.send(Request{ connection_id: idx }).await {
@@ -87,28 +92,38 @@ async fn broker_loop(rx_req: Sender<Request>, mut tx_ev: Sender<Event>, mut rx_e
                 }
                 idx = idx.wrapping_add(1);
             },
-            Event::ReadyResponse { connection_id, payload } => {
+            Event::SendingData { connection_id, payload } => {
                 if let Some(stream) = connections.get_mut(&connection_id) {
-                    spawn_write(write(stream.clone(), payload.clone()), tx_ev.clone(), connection_id);
+                    spawn_write(write(stream.clone(), payload), tx_ev.clone(), connection_id);
                 }
             },
+            Event::PackageReceived {connection_id, payload} => {
+
+            },
             Event::TimerCheckAliveConnection{} => {
-                let mut keys = vec![];
                 for (key, stream) in &connections {
                     spawn_read(read(stream.clone()), tx_ev.clone(), *key, stream.clone());
                 }
-                for key in keys {
-                    connections.remove(&key);
-                }
             },
             Event::CloseConnection { stream, connection_id } => {
-                debug!("Connection {} closed by client", stream.peer_addr()?);
+                match stream.peer_addr() {
+                    Ok(value) => {
+                        debug!("Connection {} closed by client", value);
+                    }
+                    Err(err) => {
+                        error!("{}", err);
+                    }
+                }
                 connections.remove(&connection_id);
             },
-            Event::CompletedResponse {connection_id} => {
+            Event::Disconnect {connection_id} => {
                 if let Some(stream) = connections.get_mut(&connection_id) {
-                    debug!("Response completed. Closed {} connection by server", stream.peer_addr()?);
-                    stream.shutdown(std::net::Shutdown::Both);
+                    if let Ok(value) = stream.peer_addr() {
+                        debug!("Closed {} connection by server", value);
+                    }
+                    if let Err(err) = stream.shutdown(std::net::Shutdown::Both){
+                        error!("Closed connection error. {}", err.to_string());
+                    }
                     connections.remove(&connection_id);
                 }
             }
@@ -117,7 +132,7 @@ async fn broker_loop(rx_req: Sender<Request>, mut tx_ev: Sender<Event>, mut rx_e
     Ok(())
 }
 
-async fn write(stream: Arc<TcpStream>, payload: Arc<Vec<u8>>) -> Result<()> {
+async fn write(stream: Arc<TcpStream>, payload: Vec<u8>) -> Result<()> {
     let mut stream = &*stream;
     let header_image = format!("HTTP/1.1 200 OK\x0d\x0aConnection: close\x0d\x0aMax-Age: 0\x0d\x0aExpires: 0\x0d\x0aCache-Control: no-cache, private\x0d\x0aPragma: no-cache\x0d\x0aContent-Type: text/plan\x0d\x0aContent-Length: {}\x0d\x0a\x0d\x0a", payload.len());
     stream.write_all(header_image.as_bytes()).await?;
@@ -125,7 +140,7 @@ async fn write(stream: Arc<TcpStream>, payload: Arc<Vec<u8>>) -> Result<()> {
     Ok(())
 }
 
-async fn read(stream: Arc<TcpStream>) -> Result<(usize)> {
+async fn read(stream: Arc<TcpStream>) -> Result<usize> {
     let mut stream = &*stream;
     let mut buf: [u8;1024] = [0; 1024];
     Ok(stream.read(&mut buf).await?)
@@ -137,13 +152,12 @@ async fn timer_check_alive_loop(tx_ev: Sender<Event>) -> Result<()> {
         tx_ev.send(Event::TimerCheckAliveConnection{}).await?;
         interval.as_mut().await;
     }
-    Ok(())
 }
 
 async fn executor(mut rx_req: Receiver<Request>, tx_ev: Sender<Event>) -> Result<()> {
     while let Some(req) = rx_req.next().await {
         let content = String::from("Hello world!").as_bytes().iter().map(|x|*x).collect::<Vec<u8>>();
-        tx_ev.send(Event::ReadyResponse { connection_id: req.connection_id, payload: Arc::new(content)}).await?;
+        tx_ev.send(Event::SendingData { connection_id: req.connection_id, payload: content}).await?;
     }
     Ok(())
 }
@@ -157,11 +171,14 @@ pub fn run(address: &str) -> Result<()> {
         let handle_timer = task::spawn(timer_check_alive_loop(tx_ev.clone()));
         let listener = TcpListener::bind(address).await?;
         info!("Listening on {}", address);
-        let mut incoming = listener.incoming();
+        let mut incoming = listener.incoming().log_warnings(log_accept_error).handle_errors(Duration::from_millis(500));
         while let Some(stream) = incoming.next().await {
-            let stream = stream?;
-            debug!("Accepted from: {}", stream.peer_addr()?);
-            tx_ev.send(Event::Connection {stream: Arc::new(stream)}).await?;
+            if let Ok(value) = stream.peer_addr() {
+                debug!("Accepted from: {}", value);
+            }
+            if let Err(err) = tx_ev.send(Event::OpenConnection {stream: Arc::new(stream)}).await {
+                break;
+            }
         }
         handle_broker.await?;
         handle_executor.await?;
